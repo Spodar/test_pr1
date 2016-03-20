99278a9f2a6b984473973f82f1683a73d448f69b
Spodar <unknonimus@gmail.com>
2016-03-20 20:20:10 +0200

    first commit

---
+++ js/engine.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(function() {
+	$('.slider').bxSlider();
+});
+
+$(document).ready(function() {
+    $(document).scroll(function () {
+        var scroll = $(this).scrollTop();
+        var topDist = $(".content").position();
+        if (scroll > topDist.top) {
+            $('.nav-fixed').css({"position":"fixed","top":"0"});
+        } else {
+            $('.nav-fixed').css({"position":"static","top":"auto"});
+        }
+	});
+});	
